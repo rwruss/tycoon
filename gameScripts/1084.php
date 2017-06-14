7f54216022efe1f036d842a002713eb1dc11793f
rwruss <rwruss81@gmail.com>
2017-06-13 22:54:12 -0500

    2017-06-13H

--- gameScripts/1084.php
+++ gameScripts/1084.php
@@ -25,15 +25,25 @@ $sellingFactory = loadObject($postVals[1], $objFile, 1000);
 
 // Get the recommended route node list
 $routeNum = 0;
-echo 'Load route number '.$routeNum.'<p>';
+echo 'Load route number '.$routeNum.'.  Factory '.$postVals[1].'(city '.$sellingFactory->get('region_3').') to city '.$postVals[2].'<p>';
 $routeNodes = new itemSlot($routeNum, $routeFile, 40);
 print_r($routeNodes->slotData);
 
+$routeList = $routeNodes->slotData;
+if ($sellingFactory->get('region_3') == $postVals[2]) {
+	echo 'Staying in same area';
+	$loNode = min($sellingFactory->get('region_3'), $postVals[2])-1;
+	$hiNode = max($sellingFactory->get('region_3'), $postVals[2]);
+
+	$routeNum = $loNode*($loNode+1)/2;
+	$routeList[1] = $routeNum;
+	$routeList[2] = $routeNum;
+}
 $totalMinPriceOption = [];
 $totalMinTimeOption = [];
 $legInfo = [];
-for ($r=1; $r<=sizeof($routeNodes->slotData)-1; $r++) {
-
+for ($r=1; $r<=sizeof($routeList)-1; $r++) {
+	echo 'Check route segment '.$routeList[$r].' to '.$routeList[$r+1];
 	// Look up the current best route
 	$optionList = new itemSlot($routeNum, $routeFile, 40);
 
@@ -46,8 +56,8 @@ for ($r=1; $r<=sizeof($routeNodes->slotData)-1; $r++) {
 		$ends = [0,0];
 		$endCount = 0;
 		for ($l=0; $l<10; $l++) {
-			if ($thisOption->stops[$i] == $routeNodes->slotData[$r]) $ends[0] = $i;
-			if ($thisOption->stops[$i] == $routeNodes->slotData[$r+1]) $ends[1] = $i;
+			if ($thisOption->stops[$i] == $routeList[$r]) $ends[0] = $i;
+			if ($thisOption->stops[$i] == $routeList[$r+1]) $ends[1] = $i;
 			if ($ends[0]*$ends[1] > 0) break;  // both ends have been found
 		}
 
@@ -61,7 +71,7 @@ for ($r=1; $r<=sizeof($routeNodes->slotData)-1; $r++) {
 		$sectionTime = $legDistance/$thisOption->get('speed'); // Total time = section distance / speed
 		$sectionCost = max($thisOption->get('spaceCost')*$prodSpace, $thisOption->get('weightCost')*$prodWeight);
 
-		array_push($legInfo, $routeNodes->slotData[$r], $optionList->slotData[$i], $sectionTime, $sectionCost, $thisOption->get('owner')); // route num, option #, time, cost, owner
+		array_push($legInfo, $routeList[$r], $optionList->slotData[$i], $sectionTime, $sectionCost, $thisOption->get('owner')); // route num, option #, time, cost, owner
 	}
 }
 
