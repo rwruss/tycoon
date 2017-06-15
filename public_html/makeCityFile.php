0a2472799971b94b0d40f929fae504612abc4515
rwruss <rwruss81@gmail.com>
2017-06-15 17:01:50 -0500

    2017-06-15W

--- public_html/makeCityFile.php
+++ public_html/makeCityFile.php
@@ -3,6 +3,8 @@
 $numCities = 10;
 $numRegions = 3444;
 $numNations = 196;
+
+$scenario = 1;
 /*
 $baseSlots = ($numCities+$numRegions+$numNations);
 $areaSize = $baseSlots*1000;
@@ -75,18 +77,104 @@ fseek($supplyFile, 1040000-4);
 fwrite($supplyFile, pack('i', 0));
 fclose($supplyFile);
 
+$cityList = [];
+
+// Determine the transport mode from the list file
+
+$connectListFile = fopen('c:/websites/tycoon/scenarios/'.$scenario.'/routes.csv', 'rb');
+$line = fgets($connectListFile);
+$lineInfo = exlode(',', $line);
+$transportMode = $line[3];
+// Assign city numbers to the list and store in an array, store the self distances in another array
+$cityCount = 1;
+$cityNames = [];
+while (($line = fgets($connectListFile)) !== false) {
+	$lineItems = explode(',', $line);
+	$cityList[$lineItems[0]] = $cityCount;
+	$selfDist[$lineItems[0]] = $lineItems[1];
+	$cityNames[$cityCount] = $lineItems[0];
+	$cityCount++;
+}
+
+// Determine the route connections for each city and record
+$connectDatFile = fopen('c:/websites/tycoon/scenarios/'.$scenario.'/connections.cxf', 'wb');
+fseek($connectListFile, 0);
+
+$useDistance = 0;
+$numRoutes = $cityCount*($cityCount+1)/2;
+$routeDistances = array_fill(0, $numRotues, 0);
+
+while (($line = fgets($connectListFile)) !== false) {
+	$lineItems = explode(',', $line);
+	
+	for ($i=2; $i<sizeof($lineItems); $i++) {
+		if ($cityList[$lineItems[0]] < $cityList[$lineItems[$i])
+		$thisRoute = calcRouteNum($cityList[$lineItems[0]], $cityList[$lineItems[$i]]);
+	
+		$useDistance = intval(($selfDist[$lineItems[0]] + $selfDist[$lineItems[$i]])/2);
+		$routeDistances[$thisRoute] = $useDistance;		
+	}
+}
+
+// Run a* on the rotue connections
+$nodeCost = array_fill(0, $cityCount+1, 999999);
+$pvsNode = array_fill(0, $cityCount+1, 0);
+for ($city = 1; $city <= $cityCount; $city++) {
+	$nodeCost = array_fill(0, $cityCount+1, 999999);
+	$pvsNode = array_fill(0, $cityCount+1, 0);
+	
+	$checkList = [$city];
+	while (sizeof($checkList) > 0) {
+		$pvsCity = array_shift($checkList);
+		for ($dstCity = 1; $dstCity <= $cityCount; $dstCity++) {
+			$routeNum = calcRouteNum($city, $dstCity);
+			$checkDist = $nodeCost[$pvsCity]+$useDistance[$routeNum]
+			if ($nodeCost[$dstCity] > $checkDist && $useDiatance[$routeNum] > 0) {
+				$nodeCost[$dstCity] = $checkDist;
+				$pvsNode[$dstCity] = $pvsCity;
+				$checkList[] = $dstCity;
+			}
+		}
+	}
+	
+	// Output the results for each city
+	for ($trgCity=$city; $trgCity>0; $trgCity--) {
+		echo '<p>Final destination is '.$cityNames[$trgCity].'<br>';
+		$pvsCity = $trgCity;
+		while ($pvsCity != $city) {
+			echo '--> '.$cityNames{$pvsCity].'<br>';
+			$pvsCity = $pvsNode[$pvsCity];
+		}
+	}
+	
+	// Store the results of each route for the city
+}
+
+// Save the route distance information
+fseek($routeDistFile, $numRoutes*$transportMode*4);
+fwrite($routeDistFile, packArray($routeDistances));
+
+/*
 $numRoutes = ($numCities-1)*($numCities)/2;
 $routeFile = fopen('routes.rtf', 'wb');
 fseek($routeFile, $numRoutes*40-4);
 fwrite($routeFile, pack('i', 0));
 fclose($routeFile);
-
+*/
 function packArray($data, $type='i') {
   $str = '';
   for ($i=1; $i<=sizeof($data); $i++) {
     $str = $str.pack($type, $data[$i]);
   }
   return $str;
+}
+
+function calcRouteNum($city1, $city2) {
+	$loCity = min($city1, $city2);
+	$hiCity = max($city1, $city2);
+	
+	$routeNum = ($loCity-1)*($loCity)/2 + $hiCity - $loCity;
+	return $routeNum;
 }
 
 ?>
