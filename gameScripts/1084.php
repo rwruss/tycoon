b25f3e8d703a9d31a1ba602a01353a36588c7198
rwruss <rwruss81@gmail.com>
2017-06-23 16:14:14 -0500

    2017-06-23W

--- gameScripts/1084.php
+++ gameScripts/1084.php
@@ -51,64 +51,22 @@ for ($i=0; $i<sizeof($modeChanges); $i+=2) {
 	echo 'One mode from '.$modeChanges[$i].' to '.$modeChanges[$i+1].'<br>';
 
 	// Look up available transport for each segment of the route
-	fseek($transportFile, calcRouteNum($modeChanges[$i], $modeChanges[$i+1])*4);
-}
-
-$legInfo = [];
-
-/*
-// Get the recommended route node list
-$routeNum = 0;
-echo 'Load route number '.$routeNum.'.  Factory '.$postVals[1].'(city '.$sellingFactory->get('region_3').') to city '.$postVals[2].'<p>';
-$routeNodes = new itemSlot($routeNum, $routeFile, 40);
-print_r($routeNodes->slotData);
-
-$routeList = $routeNodes->slotData;
-if ($sellingFactory->get('region_3') == $postVals[2]) {
-	echo 'Staying in same area';
-	$loNode = min($sellingFactory->get('region_3'), $postVals[2])-1;
-	$hiNode = max($sellingFactory->get('region_3'), $postVals[2]);
-
-	$routeNum = $loNode*($loNode+1)/2;
-	$routeList[1] = $routeNum;
-	$routeList[2] = $routeNum;
-}
-$totalMinPriceOption = [];
-$totalMinTimeOption = [];
-$legInfo = [];
-for ($r=1; $r<=sizeof($routeList)-1; $r++) {
-	echo 'Check route segment '.$routeList[$r].' to '.$routeList[$r+1];
-	// Look up the current best route
-	$optionList = new itemSlot($routeNum, $routeFile, 40);
-
-	// Load the best route option for each leg of the route
-	for ($i=1; $i<=sizeof($optionList->slotData); $i++) {
-		fseek($routeFile, $optionList->slotData[$i]);
-		$thisOption = new routeOpt(fread($routeFile, 100));
-
-		// Determine which stops are the end of the section in question
-		$ends = [0,0];
-		$endCount = 0;
-		for ($l=0; $l<10; $l++) {
-			if ($thisOption->stops[$i] == $routeList[$r]) $ends[0] = $i;
-			if ($thisOption->stops[$i] == $routeList[$r+1]) $ends[1] = $i;
-			if ($ends[0]*$ends[1] > 0) break;  // both ends have been found
-		}
-
-		// Calculate the total distance for this portion of the route
-		$legDistance = 0;
-		for($s=0; $s<$ends[1] - $ends[0]; $s++) {
-			$legDistance += $thisOption->stopsDist[$ends[0]+$s];
-		}
-
-		// Calc time and cost for this segment
-		$sectionTime = $legDistance/$thisOption->get('speed'); // Total time = section distance / speed
-		$sectionCost = max($thisOption->get('spaceCost')*$prodSpace, $thisOption->get('weightCost')*$prodWeight);
-
-		array_push($legInfo, $routeList[$r], $optionList->slotData[$i], $sectionTime, $sectionCost, $thisOption->get('owner')); // route num, option #, time, cost, owner
+	$legRoutes = new itemSlot(calcRouteNum($modeChanges[$i], $modeChanges[$i+1]), $tranportFile, 40);
+	for ($i=1; $i<=sizeof($legRoutes->slotData); $i++) {
+		// Load the route data
+		fseek($transportFile, $legRoutes->slotData[$i]);
+		$routeDat = fread($transportFile, 100);
+		$routeInfo = unpack('i*', substr($routeDat, 0, 40);
+		$routeStops = unpack('s*', substr($routeDat, 56, 40);
+		
+		// Determine total travel time
+		$routeDist = array_sum(array_slice($routeStops, 11);
+		$routeTime = $routeDist/$routeInfo[3];
 	}
 }
-*/
+
+$legInfo = [];
+
 fclose($objFile);
 fclose($routeFile);
 fclose($transportFile);
