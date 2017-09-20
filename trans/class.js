c5e111d20cf678cb0de5703c935a51be33874dc4
rwruss <rwruss81@gmail.com>
2017-09-20 17:04:59 -0500

    2017-09-20W

--- trans/class.js
+++ trans/class.js
@@ -1,10 +1,15 @@
 class transaction {
   constructor(dat) {
-    this.date = dat[0];
+	this.transID = dat[0];
+    this.date = dat[1];
     this.card = dat[2];
-    this.amount = dat[1]/100;
-    this.category = dat[3];
-    this.desc = dat[4];
+    this.amount = dat[3]/100;
+    this.category = dat[4];
+    this.desc = dat[5];
+	this.rowItem = null;
+	
+	let date = new Date(this.date*1000);
+	this.monthNum = calcMonthNum(date);
   }
 
   tableLine (trg) {
@@ -15,13 +20,115 @@ class transaction {
     newRow.amount = addDiv("", "transRowDate", newRow);
     newRow.category = addDiv("", "transRowDate", newRow);
     newRow.desc = addDiv("", "transRowDesc", newRow);
+	newRow.parentObj = this;
 
     let date = new Date(this.date*1000);
 
-    newRow.date.innerHTML = (date.getMonth()+1) + " / " + date.getUTCDate() + " / " + date.getFullYear();
+    newRow.date.innerHTML = (date.getUTCMonth()+1) + " / " + date.getUTCDate() + " / " + date.getUTCFullYear() + ", " + this.date;
     newRow.card.innerHTML = this.card;
     newRow.amount.innerHTML = this.amount.toFixed(2);
-    newRow.category.innerHTML = this.category;
+    newRow.category.innerHTML = categories[this.category];
     newRow.desc.innerHTML = this.desc;
+	
+	
+	newRow.addEventListener("click", function () {
+		console.log("category selection");
+		categorySelect.showOptions(this.parentObj, this.parentObj.category);
+	});
+	this.rowItem = newRow;
   }
+  
+  changeCategory(newCat) {
+	  this.category = newCat;
+	  this.rowItem.category.innerHTML = categories[newCat];
+  }
+}
+
+class optionSelect {
+	constructor(options) {
+		this.options = options;
+		this.holder = null;
+		this.subDivs = new Array();
+		this.transaction = null;
+	}
+	
+	showOptions(transItem, selected) {
+		this.selected = selected;
+		//this.subDivs = new Array();
+		this.transaction = transItem;
+		
+		console.log(this.selected);
+		let newBox;
+		if (this.holder) {
+			console.log("already made " + this.subDivs.length);
+			newBox = this.holder;
+			for (let i=0; i<this.subDivs.length; i++) {
+				this.subDivs[i].className = "optionUn";
+			}
+		} else {
+			console.log("make new");
+			newBox = addDiv("", "optionUn", contentDiv);		
+		
+			newBox.parentObj = this;
+			for (let i=0; i<categories.length; i++) {
+				let catOption = addDiv("", "optionUn", newBox);
+				catOption.itemNum = i;
+				catOption.innerHTML = categories[i];
+				catOption.addEventListener("click", function () {
+					this.parentNode.parentObj.selectOption(this.itemNum);
+
+					this.parentNode.parentObj.holder.className = "optBoxHidden";
+					//this.parentNode.parentObj.holder.parentNode.removeChild(this.parentNode.parentObj.holder);
+				});
+				this.subDivs.push(catOption);
+			}
+			this.holder = newBox;
+		}
+		newBox.className = "optBoxShow";
+		//this.subDivs[selected].innerHTML = "SELECTED";
+		this.subDivs[selected].className = "optionSel";
+		console.log(this.subDivs);
+	}
+	
+	selectOption(optionNum) {
+		this.transaction.changeCategory(optionNum);
+	}
+}
+
+class sortBox {
+	constructor (options, baseList, property) {
+		this.container = null;
+		this.optionList = options;
+		this.showOptions();
+		this.baseList = baseList;
+		this.prop = property;
+		console.log(this.optionList);
+	}
+	
+	showOptions() {
+		let newBox = addDiv("", "optionUn", contentDiv);
+		newBox.innerHTML = "OPTIONS"
+		for (let i=0; i<this.optionList.length; i+=2) {
+			let tmpDiv = addDiv("", "", newBox);
+			tmpDiv.innerHTML = this.optionList[i+1];
+			tmpDiv.itemNum = this.optionList[i];
+			tmpDiv.parentObj = this;
+			tmpDiv.addEventListener("click", function () {
+				console.log("selected " + this.itemNum);
+				this.parentObj.sortList(this.itemNum);
+			})
+		}
+	}
+	
+	sortList(itemNum) {
+		console.log("sort based on item num " + itemNum + " --> " + this.optionList[itemNum*2+1] + " and property " + this.prop);
+		let tmpA = [];
+		for (let i=0; i<this.baseList.length; i++) {
+			console.log(this.baseList[i][this.prop] + " vs " + this.optionList[itemNum*2]);
+			if (this.baseList[i][this.prop] == this.optionList[itemNum*2]) {
+				tmpA.push(i);
+			}
+		}
+		console.log(tmpA);
+	}
 }
--- trans/functions.js
+++ trans/functions.js
@@ -11,9 +11,76 @@ addDiv = function(id, useClassName, target) {
 	return newDiv;
 }
 
+async function getASync(val) {
+	let r = await loadDataPromise(val);
+	return r;
+}
+
 function init() {
 	contentDiv = document.getElementById("content");
   loadData();
+}
+
+function initSort() {
+	let monthBox = document.getElementById("monthSelect");
+	console.log(monthList);
+	monthBox.addEventListener("click", function () {
+		console.log("create sort box");
+		let boxOptions = [];
+		let monthNum, yearNum;
+		for (let i=0; i<monthList.length; i++) {
+			monthNum = monthList[i]%12;
+			yearNum = Math.floor(monthList[i]/12)+1970;
+			boxOptions.push(monthList[i], monthNames[monthNum] + " " + yearNum);
+		}
+		new sortBox(boxOptions, transactions, "monthNum");
+	});
+	let catBox = document.getElementById("catSelect");
+}
+
+function initTest() {
+	categories[0] = "None";
+	let timeSpace = 15*24*3600;
+	for (let i=0; i<20; i++) {
+		transactions.push(new transaction ([i, i*timeSpace+1, i, i, i, "item " + i]));
+		categories.push("Cat " + i);
+	}
+	contentDiv = document.getElementById("content");
+	showData(transactions, contentDiv);
+	
+	categorySelect = new optionSelect(categories);
+	loadMonths(transactions);
+	initSort();
+}
+
+function loadMonths(itemList) {
+	let date = new Date(itemList[0].date*1000);
+	let monthNum = calcMonthNum(date);
+	let loMonth = monthNum;
+	let hiMonth = monthNum;
+	console.log(itemList[0].date +" = " + monthNum + "//" + (date.getUTCFullYear()-1970)*12)
+	console.log("himonth: " + hiMonth + ", loMonth: " + loMonth);
+	for (let i=1; i<itemList.length; i++) {
+		date = new Date(itemList[i].date*1000);
+		monthNum = calcMonthNum(date);
+		
+		console.log(itemList[i].date +" = " + monthNum)
+		
+		loMonth = Math.min(loMonth, monthNum);
+		hiMonth = Math.max(hiMonth, monthNum);
+	}
+	console.log("himonth: " + hiMonth + ", loMonth: " + loMonth);
+	let numMonths = hiMonth - loMonth;
+	monthList = new Array(numMonths + 1);
+	for (i=0; i<=numMonths; i++) {
+		monthList[i] = loMonth+i;
+	}
+	console.log(monthList);
+}
+
+function calcMonthNum (date) {
+	monthNum = ((date.getUTCFullYear()-1970)*12) + date.getUTCMonth();
+	return monthNum;
 }
 
 function loadData () {
@@ -30,31 +97,28 @@ function loadData () {
 	});
 }
 
+function loadDataPromise(val) {
+	return new Promise(resolve => {
+		params = "val1="+val;
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("POST", "servItem.php?gid=0", true);
+		xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xmlhttp.onreadystatechange = function() {
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				console.log(xmlhttp.response);
+				resolve(xmlhttp.response);
+				}
+			}
+
+		xmlhttp.send(params);
+	})
+}
+
 function showData (list, trg) {
 	trg.innerHTML = "show Data";
-	for (i=0; i<list.length; i++)	 {
+	for (i=0; i<list.length; i++) {
 		list[i].tableLine(trg);
 	}
 }
 
-async function getASync(val) {
-	let r = await loadDataPromise(val);
-	return r;
-}
 
-function loadDataPromise(val) {
-		return new Promise(resolve => {
-			params = "val1="+val;
-			var xmlhttp = new XMLHttpRequest();
-			xmlhttp.open("POST", "servItem.php?gid=0", true);
-			xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-			xmlhttp.onreadystatechange = function() {
-				if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-					console.log(xmlhttp.response);
-					resolve(xmlhttp.response);
-					}
-				}
-
-			xmlhttp.send(params);
-		})
-	}
