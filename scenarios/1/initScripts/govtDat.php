5bed9795fd7b3fd6974fb457299a624e5075bdd3
rwruss <rwruss81@gmail.com>
2018-01-03 23:29:11 -0600

    2018-01-03H

--- scenarios/1/initScripts/govtDat.php
+++ scenarios/1/initScripts/govtDat.php
@@ -1,29 +1,61 @@
 <?php
-header('Content-Type: text/html; charset=utf-16', true);
-$govtFile = fopen('./govActions.dat', 'r+b');
+//header('Content-Type: text/html; charset=utf-16', true);
+$govtFile = fopen('./govActions.dat', 'w+b');
 
-fseek($govtFile, 1000);
-fwrite($govtFile, pack('i*', 0, 0));
+//fseek($govtFile, 1000);
+//$packDat = pack('C*', 3*16+12, 7*16+12, 7*16+12, 3*16+14);
+//$packDat = pack('C*', 0,  13*16 + 14);
+
 /*
-fwrite($govtFile, '{"test":test}');
+$val = 1*16*16*16*16 + 15*16*16*16 + 6*16*16 + 2*16 + 13;
+$val = 4036991149;
+echo decbin($val);
+echo '<p><hr><p>';
+$packDat = pack("N", $val);
+fwrite($govtFile, $packDat);
+
+echo $packDat;
+echo '<p><hr><p>';
+print_r(unpack('C*', $packDat));
+print_r(unpack('N', $packDat));
 */
 
-//print_r(unpack("n", $packTest));
-fwrite($govtFile, pack("n", 0xFEFF));
-$offset = 100000;
-for ($i=0; $i<20000; $i++) {
-  fwrite($govtFile, pack("n", $offset+$i));
-}
+$splitStr = pack("N", 0);
+$headDat = pack("C*", 1, 1);
+$headDat.= pack("i*", 1000, 123456);
+
+fwrite($govtFile, $headDat."Some shit that the player is called");
+fwrite($govtFile, $splitStr.$headDat."Some shit that the player is called");
+fwrite($govtFile, $splitStr.$headDat."Some shit that the player is called");
+fwrite($govtFile, $splitStr.$headDat."Some shit that the player is called");
 
 fflush($govtFile);
+fseek($govtFile, 0, SEEK_END);
+$size = ftell($govtFile);
 
 fseek($govtFile, 0);
-$str = fread($govtFile, 40000);
+$dat = fread($govtFile, $size);
 
-//$str = mb_convert_encoding($str, "UTF-16");
-//echo FxEFF;
+echo $dat;
+$list = explode($splitStr, $dat);
+print_r($list);
 
-echo $str;
-echo strlen($str);
+foreach($list as $value) {
+  echo '<hr>';
+  $itemHead = unpack("Cid/Csw/iamt/iPID", $value);
+  print_r($itemHead);
+  echo substr($value, 10);
+}
+
 fclose($govtFile);
+
+/*
+a-10
+b-11
+c-12
+d-13
+e-14
+f-15
+
+*/
 ?>
